0769195028fa5cd9f87b15ff3c77ab763cbaa946
Victor Pena <halo_rider200219@yahoo.com>
2022-10-26 23:36:17 -0700

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,47 @@
 // TODO: Declare any global variables we need
-
+let headsRolls = 0
+let tailsRolls = 0 
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    //console.log ('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+document.gotElementbyID('flip').addEventListener('click', function() {
+    let flip = Math.random()
+    console.log(flip)
+    if (flip < 0.5) {
+        document.getElementById('penny-image').src ='assets/images/penny-heads.jpg';
+        document.getElementById('message').textContent ='You Flipped Heads!'
+        numHeads += 1
+        document.getElementById('heads').textContent = numHeads 
+        document.getElementById('heads-percent')
+    } else (flip >= 0.5) {
+        document.getElementById('penny-image').src ='assets/images/penny-tails.jpg';
+        document.getElementById('message').textContent ='You flipped Tails!'
+        numTails += 1
+        document.getElementById('tails').textContent = numTails 
+        document.getElementById('tails-percent')
+
+    }
+
+    let numflips = numHeads + numTails
+    let percHeads = 0
+    let percTails = 0
+
+    if(numFlips > 0) {
+        percHeads = Math.round((percHeads/numFlips)100)
+        percTails = Math.round((percTails/numFlips)100)
+    }
+        document.getElementById('heads-percent').textContent=percHeads + '%'
+        document.getElementById('heads-percent').textContent=percTails + '%'
+})
+    
+    
+
+    
+
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
@@ -22,6 +57,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+        document.getElementById('clear').addEventListener('click', function () {
+            numHeads = 0
+            numTails = 0
 
+            document.getElementById('messages').toggleAttribute
+            document.getElementById
+            document.getElementById
+            document.getElementById
+
+        })
+        // TODO: Update the scoreboard (same logic as in flip button click handler)
+document.querySelector()
 })
+
+
+
+//console.log('Hi')
+//while (heads <= 0.5){}
+//if(Math.random() < 0.50) {
+//    heads++;
+//} else { (Math.random() >= 0.5)
+ //   tails++;
+//}
